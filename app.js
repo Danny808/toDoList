958c84361d7477c14f48e0d498b3e3ddcb934395
Daniel Ornowski <mrdanio02@gmail.com>
2021-10-19 23:04:42 +0200

    Add localsotrage

--- app.js
+++ app.js
@@ -19,11 +19,9 @@ const container = document.querySelector('.toDoListBox');
     taskContent.className = "taskContent";
     taskContainer.appendChild(taskContent);
     taskContent.innerText = task;
+    // Add localstorage
 
-    //btn container
-    // const btnContainer = document.createElement('div');
-    // btnContainer.className = 'btnContainer';
-    // taskContainer.appendChild(btnContainer);
+    saveLocalTask(task);
 
     //done button
     const doneTaskBtn =  document.createElement('button');
@@ -46,6 +44,7 @@ const container = document.querySelector('.toDoListBox');
        {
            itemToDelate = item.parentNode;
            itemToDelate.remove();
+           removeLocalTasks(item);
        }
 
        if(item.classList[0] === "doneBtn")
@@ -56,8 +55,68 @@ const container = document.querySelector('.toDoListBox');
    }
 
 
+   function saveLocalTask(item){
+       let items;
+       if(localStorage.getItem('items') === null){
+           items = [];
+       }
+       else{
+           items = JSON.parse(localStorage.getItem('items'));
+       }
+       items.push(item);
+       localStorage.setItem('items', JSON.stringify(items));
+   }
+
+   function getTasks() {
+       let items;
+       if(localStorage.getItem('items') === null){
+        items = [];
+    }
+    else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    items.forEach(function(item) {
+        //TaskDiv
+    const taskContainer = document.createElement('div');
+    taskContainer.className = 'taskContainer flex';
+    container.appendChild(taskContainer);
+    
+    //task value
+    const taskContent = document.createElement('p');  
+    taskContent.className = "taskContent";
+    taskContainer.appendChild(taskContent);
+    taskContent.innerText = item;
+    // Add localstorage
+
+    //done button
+    const doneTaskBtn =  document.createElement('button');
+    doneTaskBtn.className = 'doneBtn btn';
+    doneTaskBtn.innerHTML = '<i class="fas fa-check"></i>'
+    taskContainer.appendChild(doneTaskBtn);
+
+    //remove button
+    const removeTaskBtn = document.createElement('button');
+    removeTaskBtn.className = 'removeBtn btn';
+    removeTaskBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    taskContainer.appendChild(removeTaskBtn);
+    });
+   }
+
+   function removeLocalTasks(item){
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }
+    else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    const itemIndex = item.children[0].innerText;
+    items.splice(items.indexOf(itemIndex),1);
+    localStorage.setItem('items', JSON.stringify(items));
+   }
+
    addBtn.addEventListener('click', addNewTask);
    container.addEventListener('click', removeTask);
+   document.addEventListener('DOMContentLoaded', getTasks )
 
 
 
